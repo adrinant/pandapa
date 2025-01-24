e8eb980a3f1bed52572876d843bc15a35d85b5df
adrinant <adrinant2000@gmail.com>
2025-01-24 23:26:15 +0700

    edit supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,4 +2,9 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key are required. Please check your .env file.');
+}
+
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
